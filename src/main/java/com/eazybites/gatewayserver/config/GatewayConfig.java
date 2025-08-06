fdf7bd643f975891fa5013bd7a01387d26395a2c
joselopez <alberto.esteva88@gmail.com>
2025-08-05 21:02:50 -0600

    Corrigiendo error de falla carga de configuracion de rutas por falta de la anotacion @Bean

--- src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
+++ src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
@@ -2,11 +2,13 @@ package com.eazybites.gatewayserver.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
 
+    @Bean
     public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder
         .routes()
