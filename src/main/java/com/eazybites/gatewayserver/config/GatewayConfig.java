0778d9270d0cf90af5d748a71c35ad3e7db84761
joselopez <alberto.esteva88@gmail.com>
2025-08-05 21:44:00 -0600

    Se agrego filtro provisto por Spring que agrega a la respuesta un header con la fecha y hora de respuesta

--- src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
+++ src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
@@ -1,5 +1,7 @@
 package com.eazybites.gatewayserver.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,15 +16,18 @@ public class GatewayConfig {
         .routes()
         .route(p -> p
           .path("/eazybank/accounts/**")
-          .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+          .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
+            .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
           .uri("lb://ACCOUNTS"))
         .route(p -> p
           .path("/eazybank/loans/**")
-          .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}"))
+          .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
+            .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
           .uri("lb://LOANS"))
         .route(p -> p
           .path("/eazybank/cards/**")
-          .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}"))
+          .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+            .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
           .uri("lb://CARDS"))    
         .build();
     }
