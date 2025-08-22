81412e2474b8eac1d94828bbe09be8c6423d107f
joselopez <alberto.esteva88@gmail.com>
2025-08-22 00:31:25 -0600

    Implementando Retry Pattern para el servicio loans

--- src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
+++ src/main/java/com/eazybites/gatewayserver/config/GatewayConfig.java
@@ -1,11 +1,13 @@
 package com.eazybites.gatewayserver.config;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
@@ -28,6 +30,18 @@ public class GatewayConfig {
           .path("/eazybank/loans/**")
           .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
             .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+            // recomendado usar retry con solo con metodos idempotentes
+            .retry(retryConfig -> retryConfig.setRetries(3)
+              .setMethods(HttpMethod.GET)
+              // parametros:
+              // Despues de que el request falla en responder por demora basado en el valor especificado en application.yml (responseTimeout)
+              // Se realizan reintentos de conexion
+              // firstBackoff(backoff mínimo): Tiempo inicial de reintento (el primer reintento se realiza a los 100ms )
+              // maxBackoff(backoff máximo): Tope de tiempo a esperar para realizar el reintento
+              // factor(multiplicador exponencial): delay(n) = initialDelay × (multiplicador ^ (n-1)) n= numero de reintento (para calcular el retraso en el intento)
+              // jitter (aleatoriedad en el backoff): Si está en true, introduce una variación aleatoria en los tiempos
+                  //de espera para evitar que muchos clientes reintenten al mismo tiempo (efecto avalancha).
+              .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
           )
           .uri("lb://LOANS"))
 
